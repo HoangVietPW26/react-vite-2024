b34f2c428f441dc9f6dc4ebf4bd49fe710361bfb
HoangVietPW26 <nhviet26@gmail.com>
2024-07-29 14:49:52 +0700

    var with jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,17 @@ import './styles.css'
 
 const MyComponent = () => {
 
+    const viet = "viet"
+    const viet2 = {
+      name: "viet",
+      age: 24
+    }
+    const viet3 = [1,2,3]
+
     return (
       <>
-      <div className=""> Nguyen Hoang Viet Update </div>
+      <div className=""> {JSON.stringify(viet2)} & Nguyen Hoang Viet Update </div>
+      <div>{console.log("MAGIC")}</div>
       <div className="child" style={{borderRadius: "10px"}}> child </div>
       </>
     )
